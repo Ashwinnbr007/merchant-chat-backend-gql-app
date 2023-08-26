34fecde6e626f11ab38b7a6a6752fb252182ae53
Ashwinnbr007 <ashwinnambiarbusiness@gmail.com>
2023-08-26 09:45:45 +0530

    postgres typeorm, Graphql, Apollo initialised in the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,30 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { MessageModule } from './message/message.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
 
 @Module({
-  imports: [UserModule, MessageModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'sus17251',
+      database: 'merchant-chat-backend-gql-app',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+    UserModule,
+    MessageModule,
+  ],
   controllers: [],
   providers: [],
 })
