3fa53cf56ecda712dcbf96120814c44ff553a8f9
Ashwinnbr007 <ashwinnambiarbusiness@gmail.com>
2023-08-28 21:16:50 +0530

    Added env files

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,18 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { join } from 'path';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DATABASE_HOST,
       port: 5432,
-      username: 'postgres',
-      password: 'sus17251',
-      database: 'merchant-chat-backend-gql-app',
+      username: process.env.DATABASE_USER_NAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_DB,
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,
     }),
