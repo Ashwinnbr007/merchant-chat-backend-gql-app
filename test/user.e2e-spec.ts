504fe12e8c3b507514eb3b011ebb8143ca484d6b
Ashwinnbr007 <ashwinnambiarbusiness@gmail.com>
2023-08-29 10:11:24 +0530

    Added tests for User entity

--- test/user.e2e-spec.ts
+++ test/user.e2e-spec.ts
@@ -1,12 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, NotFoundException } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
 
 describe('UserResolver (e2e)', () => {
   let app: INestApplication;
-let server;
-    let allTestsCompleted = false;    
+  let server;
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -19,7 +18,10 @@ let server;
   });
 
   describe('User Endpoints', () => {
+    // Storing a new testUserId as we will be deleting the instance of the
+    // testUser after our tests as I am not having a separate database for test.
     let testUserId;
+
     // Tests creation of a new user
     it('should return a new user', async () => {
       const response = await request(server)
@@ -47,6 +49,54 @@ let server;
       testUserId = data.createUser.userId;
     });
 
+    // Test updation of a User
+    it('should update a user', async () => {
+      const response = await request(server)
+        .post('/graphql')
+        .send({
+          query: `
+            mutation {
+                updateUser(updateUserInput: {
+                  userName: "Test user 2",
+                  userId: ${testUserId},
+                }){
+                  userId,
+                  userName
+                }
+              }
+          `,
+        });
+      const { data } = response.body;
+      expect(data).toEqual({
+        updateUser: {
+          userId: testUserId,
+          userName: 'Test user',
+        },
+      });
+    });
+
+    it('should not update a user', async () => {
+      const response = await request(server)
+        .post('/graphql')
+        .send({
+          query: `
+              mutation {
+                  updateUser(updateUserInput: {
+                    userName: "Test user 2",
+                    userId: ${testUserId + 500},
+                  }){
+                    userId,
+                    userName
+                  }
+                }
+            `,
+        });
+      const { errors } = response.body;
+      expect(errors).toHaveLength(1);
+      expect(errors[0].message).toBe(
+        `User with userId ${testUserId + 500} not found`,
+      );
+    });
     // Tests deletion of a user
     it('should delete a user', async () => {
       const response = await request(server)
